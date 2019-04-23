a834d9e9624a393c7dc5018650a1dcd38b2a672f
Sathish Nagarajan <sathishnagarajan603@gmail.com>
2019-04-23 21:49:18 +0530

    changes 230419

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -21,18 +21,15 @@ export default class Header extends Component {
                     <div className="row banner">
                         <div className="banner-text">
                         <h1 className="responsive-headline">I'm {resumeData.name}.</h1>
-                        <h3>I'm a {resumeData.role}. <span>illustrator</span> and <span>webdesigner</span> creating awesome and
-                            effective visual identities for companies of all sizes around the globe. Let's <a className="smoothscroll" href="#about">start scrolling</a>
+                        <h3>I'm a {resumeData.role}, <span>UI developer</span> and <span>Web Designer</span> focused on crafting clean and user friendly experiences. Experienced UI Developer with a demonstrated history of working in IT Software. Skilled in converting PSDs to HTML, converting HTML to WordPress templates, Code Emails and Landing Pages. Designing PSD as per wire-frame. Let's <a className="smoothscroll" href="#about">start scrolling</a>
                             and learn more <a className="smoothscroll" href="#about">about me</a>.</h3>
                         <hr />
                         <ul className="social">
-                            <li><a href="www.google.com"><i className="fa fa-facebook" /></a></li>
-                            <li><a href="www.google.com"><i className="fa fa-twitter" /></a></li>
-                            <li><a href="www.google.com"><i className="fa fa-google-plus" /></a></li>
-                            <li><a href="www.google.com"><i className="fa fa-linkedin" /></a></li>
-                            <li><a href="www.google.com"><i className="fa fa-instagram" /></a></li>
-                            <li><a href="www.google.com"><i className="fa fa-dribbble" /></a></li>
-                            <li><a href="www.google.com"><i className="fa fa-skype" /></a></li>
+                            <li><a href="https://www.facebook.com/satishnagarajan603"><i className="fa fa-facebook" /></a></li>
+                            <li><a href="https://twitter.com/Sathish603"><i className="fa fa-twitter" /></a></li>
+                            <li><a href="https://www.linkedin.com/in/satish-nagarajan-55384b91/"><i className="fa fa-linkedin" /></a></li>
+                            <li><a href="https://www.instagram.com/sathish603/"><i className="fa fa-instagram" /></a></li>
+                            <li><a href="https://join.skype.com/invite/SQ5qikG2ZrHo"><i className="fa fa-skype" /></a></li>
                         </ul>
                         </div>
                     </div>
--- src/resumeData.js
+++ src/resumeData.js
@@ -1,8 +1,6 @@
-// import React, {Component} from 'react';
-
 let resumeData = {
     "imagebaseurl":"http://localhost:3000/",
     "name": "Satish Nagarajan",
-    "role": "Frontend Developer"
+    "role": "Front End Developer"
     }
   export default resumeData
